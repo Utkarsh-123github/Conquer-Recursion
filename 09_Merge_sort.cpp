49657b1883b210d7cb4fb678b8b0a0ba4f7d9d39
Utkarsh Tiwari <utk0703@gmail.com>
2024-12-05 11:12:38 +0530

    Merge sort updated

--- 09_Merge_sort.cpp
+++ 09_Merge_sort.cpp
@@ -47,12 +47,12 @@ void merge(vector<int>&arr, int low, int mid, int high){
 }
 
 void mergeSort(vector<int>&arr, int low, int high){
-    if(low<high){
-        int mid = low + (high-low)/2;
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid+1, high);
-        merge(arr, low, mid, high);
-    }
+    if(low>=high)return;
+    int mid = low + (high-low)/2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+     
 }
 
 int main(){
